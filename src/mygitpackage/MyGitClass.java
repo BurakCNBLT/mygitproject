5c1d5fdca20bba551d401e6ac8a88ace62f012b1
BurakCNBLT <81869269+BurakCNBLT@users.noreply.github.com>
2021-04-04 18:21:57 +0300

    Update MyGitClass.java
    GitHub.com'daki yeni kodum

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -4,9 +4,9 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("My second commit");
-		System.out.println("My third commit");
-		System.out.println("My 4th commit");
-		System.out.println("My 5th commit");
+		System.out.println("My third commit");//
+		System.out.println("My 4th commit");//
+		System.out.println("My 5th commit");//
 		System.out.println("uzak sunucudaki kodum");//GitHub.com'daki yeni kodum
 	}
 
